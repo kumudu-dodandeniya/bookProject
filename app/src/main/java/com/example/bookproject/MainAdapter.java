03ce89a7ddaddde0ae60fe4f17e12191351aaaf3
Kumudu Dodandeniya <it20089672@my.sliit.lk>
2021-09-26 00:47:41 +0530

    Update and Delete

--- app/src/main/java/com/example/bookproject/MainAdapter.java
+++ app/src/main/java/com/example/bookproject/MainAdapter.java
@@ -1,10 +1,15 @@
 package com.example.bookproject;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,9 +17,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
+import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.ViewHolder;
 import com.squareup.picasso.Picasso;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -32,7 +45,7 @@ public class MainAdapter extends FirebaseRecyclerAdapter<MainModel,MainAdapter.m
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull @NotNull myViewHolder holder, int position, @NonNull @NotNull MainModel model) {
+    protected void onBindViewHolder(@NonNull @NotNull myViewHolder holder, final int position, @NonNull @NotNull MainModel model) {
 
         holder.name.setText(model.getName());
         holder.description.setText(model.getDescription());
@@ -42,6 +55,91 @@ public class MainAdapter extends FirebaseRecyclerAdapter<MainModel,MainAdapter.m
               //  .circleCrop().error(R.drawable.common_google_signin_btn_icon_dark_normal)
                 //.into(holder.img);
         Picasso.get().load(model.getBurl()).into(holder.img);
+
+
+        holder.btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final DialogPlus dialogPlus = DialogPlus.newDialog(holder.img.getContext())
+                        .setContentHolder(new ViewHolder(R.layout.update_popup))
+                        .setExpanded(true,1200).create();
+                //dialogPlus.show();
+
+                View view = dialogPlus.getHeaderView();
+
+                EditText name = view.findViewById(R.id.txtName);
+                EditText description = view.findViewById(R.id.txtDescription);
+                EditText price = view.findViewById(R.id.txtPrice);
+                EditText burl = view.findViewById(R.id.txtImageUrl);
+
+                Button btnUpdate = view.findViewById(R.id.btnUpdate);
+
+                name.setText(model.getName());
+                description.setText(model.getDescription());
+                price.setText(model.getPrice());
+                burl.setText(model.getBurl());
+
+                dialogPlus.show();
+
+                btnUpdate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Map<String,Object> map = new HashMap<>();
+                         map.put("name", name.getText().toString());
+                        map.put("description", description.getText().toString());
+                        map.put("price", price.getText().toString());
+                        map.put("burl", burl.getText().toString());
+
+                        FirebaseDatabase.getInstance().getReference().child("book").child(getRef(position).getKey()).updateChildren(map)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Toast.makeText(holder.name.getContext(),"Data Updated Successfully", Toast.LENGTH_SHORT).show();
+                                        dialogPlus.dismiss();
+
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull @NotNull Exception e) {
+                                Toast.makeText(holder.name.getContext(),"Error While Updating..", Toast.LENGTH_SHORT).show();
+                                dialogPlus.dismiss();
+
+                            }
+                        });
+
+                    }
+                });
+
+            }
+        });
+
+        holder.btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(holder.name.getContext());
+                builder.setTitle("Are you Sure");
+                builder.setMessage("Deleted data can't be Undo..");
+
+                builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        FirebaseDatabase.getInstance().getReference().child("book")
+                                .child(getRef(position).getKey()).removeValue();
+
+                    }
+                });
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(holder.name.getContext(), "Cancelled", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.show();
+            }
+        });
+
+
 
 
     }
@@ -60,6 +158,8 @@ public class MainAdapter extends FirebaseRecyclerAdapter<MainModel,MainAdapter.m
         ImageView img;
         TextView name,description,price;
 
+        Button btnEdit,btnDelete;
+
         public myViewHolder(@NonNull @NotNull View itemView) {
             super(itemView);
 
@@ -67,6 +167,9 @@ public class MainAdapter extends FirebaseRecyclerAdapter<MainModel,MainAdapter.m
             name = (TextView) itemView.findViewById(R.id.nametext);
             description = (TextView) itemView.findViewById(R.id.descriptiontext);
             price = (TextView) itemView.findViewById(R.id.pricetext);
+
+            btnEdit = (Button)itemView.findViewById(R.id.btnEdit);
+            btnDelete = (Button)itemView.findViewById(R.id.btnDelete);
 
         }
 
