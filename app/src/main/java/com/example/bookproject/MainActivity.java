177e4a5fa95ee7a4e81561ae2a64e3b8b6bbadc1
Kumudu Dodandeniya <it20089672@my.sliit.lk>
2021-09-25 22:40:20 +0530

    Add new button

--- app/src/main/java/com/example/bookproject/MainActivity.java
+++ app/src/main/java/com/example/bookproject/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.FirebaseDatabase;
@@ -44,4 +47,42 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
         mainAdapter.stopListening();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.search,menu);
+        MenuItem item = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView)item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                txtSearch(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                txtSearch(query);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void txtSearch(String str){
+
+        FirebaseRecyclerOptions<MainModel> options =
+                new FirebaseRecyclerOptions.Builder<MainModel>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("book").orderByChild("name").startAt(str).endAt(str+"~"),MainModel.class)
+                        .build();
+
+        mainAdapter = new MainAdapter(options);
+        mainAdapter.stopListening();
+        recyclerView.setAdapter(mainAdapter);
+    }
+
+
 }
